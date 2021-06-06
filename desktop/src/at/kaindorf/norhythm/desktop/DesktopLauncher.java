9c679a69cc532d19fb2cecf85b64504a118f871b
oujian <oujian.zhang@outlook.com>
2021-06-06 17:24:42 +0200

    first_commit

---
+++ desktop/src/at/kaindorf/norhythm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.kaindorf.norhythm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.kaindorf.norhythm.RhythmGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RhythmGame(), config);
+	}
+}
